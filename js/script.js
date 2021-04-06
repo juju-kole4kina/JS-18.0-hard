6dcc1f00014808fc2a9e565da305e03884084cf1
Juliya Koltsova <juliyaasking009@gmail.com>
2021-04-06 23:59:03 +0200

    lesson04 done

--- js/script.js
+++ js/script.js
@@ -1,47 +1,23 @@
 'use strict';
 
-//1. 
+// FUNCTION
 
-let lang = prompt('ru or en?');
+const foo = function (arg) {
+   let res = '';
+   if (typeof arg === 'string') {
+      const maxLength = 30;
+      const str = arg.trim();
+      if (str.length > maxLength) {
+         res = str.substr(0, maxLength) + '...';
+      } else {
+         res = str;
+      }
+   }
+   return res;
+};
 
-//if
+// CONSOLE OUTPUT
 
-if (lang === 'ru') {
-   console.log('Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс');
-} else if (lang === 'en') {
-   console.log('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
-} else {
-   console.log('Что-то пошло не так');
-}
-
-//switch
-
-switch (lang) {
-   case 'ru':
-      console.log('Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс');
-      break;
-   case 'en':
-      console.log('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
-      break;
-   default:
-      console.log('Что-то пошло не так');
-}
-
-//array
-
-let langArr = [];
-
-langArr['ru'] = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-langArr['en'] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-//let lang = 'ru';
-
-console.log(langArr[lang]);
-
-//2.
-
-let namePerson = prompt('What is your name?');
-
-console.log((namePerson === 'Артём') ? 'Директор' :
-   (namePerson === 'Максим') ? 'Преподаватель' :
-      'Студент');
+console.log('foo(): ', foo(10));
+console.log('foo(): ', foo('Текст'));
+console.log('foo(): ', foo('Оооооооооооооооооооочень длинный текст'));
