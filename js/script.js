b850e5b36351e8c80faf452d091b475663a58c3a
Juliya Koltsova <juliyaasking009@gmail.com>
2021-04-10 00:48:14 +0200

    lesson05 done

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 'use strict';
+// VARIABLES
+
+let arr = ['1234', '2345', '2456', '3124', '43231', '7643', '4980'];
 
 // FUNCTION
 
-const foo = function (arg) {
-   let res = '';
-   if (typeof arg === 'string') {
-      const maxLength = 30;
-      const str = arg.trim();
-      if (str.length > maxLength) {
-         res = str.substr(0, maxLength) + '...';
-      } else {
-         res = str;
+let exOne = function () {
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+         console.log(arr[i]);
       }
    }
-   return res;
 };
+
+let exTwo = function (num) {
+   let arr = [];
+
+   if (num !== 1) {
+      arr.push(1);
+      for (let j = 2; j * j <= num; j++) {
+         if (num % j === 0) {
+            arr.push(j);
+         }
+      }
+   }
+   arr.push(num);
+   return arr;
+};
+
+let callExTwo = function () {
+   for (let i = 1; i <= 100; i++) {
+      let n = exTwo(i);
+
+      if (n.length <= 2) {
+         console.log(`${i}: Делители этого числа: ${n.join(' и ')}`);
+      }
+   }
+};
+
+// FUNCTION CALL
+exOne();
+callExTwo();
 
 // CONSOLE OUTPUT
 
-console.log('foo(): ', foo(10));
-console.log('foo(): ', foo('Текст'));
-console.log('foo(): ', foo('Оооооооооооооооооооочень длинный текст'));
+
