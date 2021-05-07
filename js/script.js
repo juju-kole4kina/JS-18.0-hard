3ff27022e7ddc8884f5cc2eeb760af9261d87b1d
Juliya Koltsova <juliyaasking009@gmail.com>
2021-05-07 18:01:22 +0200

    lesson07-hard done

--- js/script.js
+++ js/script.js
@@ -1,47 +1,27 @@
 'use strict';
 // VARIABLES
 
-let arr = ['1234', '2345', '2456', '3124', '43231', '7643', '4980'];
+let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-// FUNCTION
+let day = new Date();
+let numDay = day.getDay();
+if (numDay === 0) {
+   numDay = 6;
+} else {
+   numDay--;
+}
 
-let exOne = function () {
-   for (let i = 0; i < arr.length; i++) {
-      if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
-         console.log(arr[i]);
-      }
+week.forEach(function (day, i) {
+   let str = day;
+   if (i === numDay) {
+      str = `<b>${day}</b>`;
+   } else {
+      str = `${day}`;
    }
-};
-
-let exTwo = function (num) {
-   let arr = [];
-
-   if (num !== 1) {
-      arr.push(1);
-      for (let j = 2; j * j <= num; j++) {
-         if (num % j === 0) {
-            arr.push(j);
-         }
-      }
+   if (i === 5 || i === 6) {
+      str = `<i>${str}</i>`;
    }
-   arr.push(num);
-   return arr;
-};
-
-let callExTwo = function () {
-   for (let i = 1; i <= 100; i++) {
-      let n = exTwo(i);
-
-      if (n.length <= 2) {
-         console.log(`${i}: Делители этого числа: ${n.join(' и ')}`);
-      }
-   }
-};
-
-// FUNCTION CALL
-exOne();
-callExTwo();
-
-// CONSOLE OUTPUT
+   document.body.insertAdjacentHTML('beforeend', `<div>${str}</div>`);
+});
 
 
