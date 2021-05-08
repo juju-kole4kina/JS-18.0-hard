94ae8087b12ceee522f06a2d1b09147a2f122761
Juliya Koltsova <juliyaasking009@gmail.com>
2021-05-08 03:27:11 +0200

    lesson09-hard done

--- js/script.js
+++ js/script.js
@@ -1,27 +1,48 @@
 'use strict';
 // VARIABLES
 
-let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let timeDisplay = function () {
+   let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+   let month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября',
+      'октября', 'ноября', 'декабря'];
+   let date = new Date();
 
-let day = new Date();
-let numDay = day.getDay();
-if (numDay === 0) {
-   numDay = 6;
-} else {
-   numDay--;
-}
+   let addZero = function (elem) {
+      if (String(elem).length === 1) {
+         return '0' + elem;
+      } else {
+         return String(elem);
+      }
+   };
+   let changeEnding = function (num, timeElem = '') {
+      let textVariant = (timeElem === 'h' ? ['час', 'часа', 'часов'] :
+         timeElem === 'm' ? ['минута', 'минуты', 'минут'] :
+            ['секунда', 'секунды', 'секунд']);
+      let n = num % 10;
+      return num > 4 && num < 20 ? num + textVariant[2] :
+         n === 1 ? num + textVariant[0] :
+            n > 1 && n < 5 ? num + textVariant[1] :
+               num + textVariant[2];
+   };
+   let textTime = 'Сегодня ' + week[date.getDay()] + ' ' + date.getDay() + ' ' + month[date.getMonth()] +
+      ' ' + date.getFullYear() + ' года,' + ' ' + changeEnding(date.getHours(), 'h') +
+      ' ' + changeEnding(date.getMinutes(), 'm') + ' ' + changeEnding(date.getSeconds());
+   let time = addZero(date.getHours()) + ':' + addZero(date.getMinutes()) +
+      ':' + addZero(date.getSeconds()) + ' ' + addZero(date.getDay()) + '.' +
+      addZero(date.getMonth()) + '.' + date.getFullYear();
 
-week.forEach(function (day, i) {
-   let str = day;
-   if (i === numDay) {
-      str = `<b>${day}</b>`;
-   } else {
-      str = `${day}`;
-   }
-   if (i === 5 || i === 6) {
-      str = `<i>${str}</i>`;
-   }
-   document.body.insertAdjacentHTML('beforeend', `<div>${str}</div>`);
-});
+   document.querySelector('.text-time').textContent = textTime;
+   document.querySelector('.time').textContent = time;
 
+};
 
+let elem = document.createElement('div');
+elem.classList.add('text-time');
+document.body.appendChild(elem);
+
+elem = document.createElement('div');
+elem.classList.add('time');
+document.body.appendChild(elem);
+console.dir(elem);
+
+setInterval(timeDisplay, 1000);
